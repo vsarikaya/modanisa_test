ab7327504f4c2fec691fbbec71b6aaaa1e6a8a79
Volkan Sarıkaya <volkan.sarikaya@modanisa.com>
2019-06-28 14:25:13 +0300

    Version 1.1.20

--- bamboo.sh
+++ bamboo.sh
@@ -2,24 +2,41 @@
 
 set -o errexit
 
+EB_APPLICATION_NAME="modanisa_external"
+S3_BUCKET="s3://elasticbeanstalk-eu-central-1-142903308389"
+S3_BUCKET_FOLDER="$S3_BUCKET/$EB_APPLICATION_NAME"
+
 GIT_TAG_VERSION=$(git describe --abbrev=0 --tags 2> /dev/null)
 if [ -z "$GIT_TAG_VERSION" ]; then
     echo "You should set new version for deploy (GIT doesnt have any tag)"
     exit 1
 fi
 
-EB_VERSION="$(aws elasticbeanstalk describe-application-versions --application-name modanisa_external \
+EB_VERSION="$(aws elasticbeanstalk describe-application-versions --application-name $EB_APPLICATION_NAME \
             | python -c "import json,sys;obj=json.load(sys.stdin);print obj['ApplicationVersions'][0]['VersionLabel'];" \
             | sed -r "s/Version //" 2>&1)"
 
-# Alternative
-# EB_VERSION="$(eb status | grep -oP "(?<=Deployed Version: Version )([A-Za-z0-9.]+)(?=$)")"
 
 if [ "$GIT_TAG_VERSION" = "$EB_VERSION" ]; then
     echo "No have any new tag"
     exit 1
 fi
 
-eb deploy -l "Version $GIT_TAG_VERSION"
+# Compress all files with git tag
+echo "Compressing files for $GIT_TAG_VERSION"
+UPLOAD_FILE_NAME="Version ${GIT_TAG_VERSION}.zip"
+zip "$UPLOAD_FILE_NAME" -r * .[^.]*
 
+CHECK_EXISTS_S3_FILE=$(aws s3 ls $S3_BUCKET_FOLDER/$UPLOAD_FILE_NAME | wc -l)
+if [ "$CHECK_EXISTS_S3_FILE" -ne 0 ]; then
+    echo "Version already uploaded"
+    exit 1
+fi
 
+# Uploading S3
+echo "Uploading to S3 Bucket"
+aws s3 cp "$UPLOAD_FILE_NAME" "$S3_BUCKET_FOLDER/"
+
+# Install new version to ElasticBeanstalk
+aws elasticbeanstalk create-application-version --application-name "$EB_APPLICATION_NAME" \
+    --version-label "$UPLOAD_FILE_NAME" --source-bundle S3Bucket="$S3_BUCKET",S3Key="$EB_APPLICATION_NAME/$UPLOAD_FILE_NAME"
